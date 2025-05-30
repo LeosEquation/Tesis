c067d3506570fe7f64e82bd829716b5bb0a9b764
Leonel Mayorga López <leoml141.lml@gmail.com>
2025-05-30 17:37:23 -0600

    Change structures of equilibria

--- src/objects/equilibria.jl
+++ src/objects/equilibria.jl
@@ -2,31 +2,34 @@ struct LimitPoint{U<:Real}
     x::Array{U, 1}
     vector::Array{Complex{U}, 1}
     value::Complex{U}
+    nullvec::Array{U, 1}
 end
 
-function LimitPoint(x::Vector{T}, v::Vector{T}, p::T) where T<:Real
-    LimitPoint(x, complex.(v), complex(p))
+function LimitPoint(x::Vector{T}, v::Vector{T}, p::T, nullvec::Array{T, 1}) where T<:Real
+    LimitPoint(x, complex.(v), complex(p), nullvec)
 end
 
 struct HopfPoint{U<:Real}
     x::Array{U, 1}
     vector::Array{Complex{U}, 1}
     value::Complex{U}
+    nullvec::Array{U, 1}
 end
 
-function HopfPoint(x::Vector{T}, v::Vector{T}, p::T) where T<:Real
-    HopfPoint(x, complex.(v), complex(p))
+function HopfPoint(x::Vector{T}, v::Vector{T}, p::T, nullvec::Array{T, 1}) where T<:Real
+    HopfPoint(x, complex.(v), complex(p), nullvec)
 end
 
 struct BranchPoint{U<:Real}
     x::Array{U, 1}
     vector::Array{Complex{U}, 1}
     value::Complex{U}
+    nullvec::Array{U, 1}
     dir::Array{U, 1}
 end
 
-function BranchPoint(x::Vector{T}, v::Vector{T}, p::T, dir::Vector{T}) where T<:Real
-    BranchPoint(x, complex.(v), complex(p), dir)
+function BranchPoint(x::Vector{T}, v::Vector{T}, p::T, nullvec::Array{T, 1}, dir::Vector{T}) where T<:Real
+    BranchPoint(x, complex.(v), complex(p), nullvec, dir)
 end
 
 struct EquilibriumBranch{U<:Real}
@@ -35,7 +38,7 @@ struct EquilibriumBranch{U<:Real}
     lp::Array{LimitPoint{U}, 1}
     hp::Array{HopfPoint{U}, 1}
     bp::Array{BranchPoint{U}, 1}
-    stbl::Array{Bool, 1}
+    stbl::Array{Int, 1}
 end
 
 function Base.show(io::IO, obj::EquilibriumBranch)
